43edcbff55528ab7e83fccce359d75d668961328
rs-patn <patrick.naegele@rackspace.com>
2022-04-27 10:56:28 -0400

    Adding baseline script

---
+++ migrations/B001_20220427100462__poctest_details_prod_baseline.sql
@@ -0,0 +1,15 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[poctest_details]'
+GO
+CREATE TABLE [dbo].[poctest_details]
+(
+[firstName] [varchar] (25) NULL,
+[lastName] [varchar] (25) NULL,
+[age] [tinyint] NULL,
+[city] [varchar] (25) NULL,
+[state] [char] (2) NULL
+)
+GO
