1706baf285d4f2745120fdaedd2a3124903d8a54
rs-patn <patrick.naegele@rackspace.com>
2022-04-27 10:05:15 -0400

    Adding SQL scripts

---
+++ schema-model/Tables/dbo.poctest_details.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[poctest_details]
+(
+[firstName] [varchar] (25) NULL,
+[lastName] [varchar] (25) NULL,
+[age] [tinyint] NULL,
+[city] [varchar] (25) NULL,
+[state] [char] (2) NULL
+)
+GO
